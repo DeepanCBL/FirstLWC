365cafb9be96d0e372cfb39e5c0b0aaf63192a2a
Deepan <deepanchakaravarthy328@gmail.com>
2023-10-12 13:17:06 +0530

    first commit

---
+++ force-app/main/default/lwc/firstLWC/firstLWC.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class FirstLWC extends LightningElement {
+    name = 'deepan';
+    gender = 'male';
+    role = 'salesforce developer';
+    salary = '$10000';
+}
